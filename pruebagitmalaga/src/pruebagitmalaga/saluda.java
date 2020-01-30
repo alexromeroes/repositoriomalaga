aa31e97e53b9fdf00e0d66784167332f301cce5b
Alex <Alex@DESKTOP-SF7MT98>
2020-01-30 18:55:11 +0100

    Version 2 Hemos añadido la funcionalidad X

--- pruebagitmalaga/src/pruebagitmalaga/saluda.java
+++ pruebagitmalaga/src/pruebagitmalaga/saluda.java
@@ -4,7 +4,7 @@ public class saluda {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola 5");
+		System.out.println("Hola esta es la segunda version del dia segundo");
 
 	}
 
